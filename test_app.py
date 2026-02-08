4aa019367c938429969095c5b32a6a4137002e8f
Uladzislau54529 <vsyrevic@gmail.com>
2026-02-08 20:07:35 +0100

    tests 3

--- test_app.py
+++ test_app.py
@@ -1,6 +1,14 @@
+from fastapi.testclient import TestClient
 from main import main
 
-def test_movies():
-    client = main.test_client()
+client = TestClient(main)
+
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
+    assert response.json() == {"message": "Project 2 is running"}
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
